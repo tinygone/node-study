b549c7b636e643a42551859fcddd6abb88f94ac0
tinygone <tinygone@qq.com>
2017-09-23 15:33:07 +0800

    post & get

---
+++ getpost/getServer2.js
@@ -0,0 +1,18 @@
+/**
+ * Created by liqigang on 2017/9/23.
+ */
+var http = require('http');
+var url = require('url');
+var util = require('util');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+// 文件编码未指定，会导致部分浏览器显示乱码
+    // 解析 url 参数
+    var params = url.parse(req.url, true).query;
+    res.write("网站名：" + params.name);
+    res.write("\n");
+    res.write("网站 URL：" + params.url);
+    res.end();
+
+}).listen(3000);
